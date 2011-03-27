9d5f8112ed08516d646cb4a13fd493759d78a442
max99x <max99x@gmail.com>
2011-03-27 10:53:50 +0200

    Polish; docs; removed stdin label and hack.

--- jqconsole.js
+++ jqconsole.js
@@ -1,7 +1,7 @@
-(function ($){
+(function ($) {
 
-$.fn.console = function(options){
- 
+$.fn.console = function(options) {
+  // Setup DOM elements.
   var $history = $('<div/>').appendTo(this);
   var $typer = $('<textarea/>',{style: "position:absolute;left:-99999px"}).appendTo(this);
   var $prompt = $('<div/>', {tabindex:1, id:"jq-console-prompt"}).appendTo(this);
@@ -12,64 +12,68 @@ $.fn.console = function(options){
   var $historyItem = $('<div><pre></pre></div>');
   var $stdoutItem = $historyItem.clone().addClass('out');
   var $paster = $('<textarea/>');
+
+  // Setup other stuff.
+  // TODO(max99x): Elaborate.
   var currentOut = null;
   var history = [];
   var history_index = 0;
   var that = this;
 
+  // Setup configurable settings.
   var settings = {
-    label: '>>>',
+    label: 'jq-console> ',
     handler: $.noop,
-    greetings: "jsrepl"
+    greetings: "Welcome to jq-console!"
   };
-
   $.extend(settings, options);
 
-  $('<div/>').text(settings.greetings).appendTo($history);
+  // Write greeting.
+  $('<pre/>').text(settings.greetings).appendTo($history);
+  // Write prompt.
   $promptLabel.text(settings.label);
 
-  //mozilla prefers keypress always
-  var keydown = $.browser.mozilla ? 'keypress' : 'keydown';
-//start prompt bindings
-  this.click(function(){
+  // Mouse prompt bindings.
+  this.click(function() {
     $typer.focus();
   });
-  $prompt.focus(function(){
+  $prompt.focus(function() {
     $typer.focus();
   });
-  $typer
-    .keypress(function(e){
-      //IE & Chrome captures charecters and return
-      //Mozilla captures all
-      //IE captures on keyCode
-      
-      if ($.browser.mozilla && !e.charCode) return;
-      charCode = e.charCode || e.keyCode;
-      if (charCode == 13) return;
-      scrollToEnd();
+
+  // Keyboard prompt bindings.
+  $typer.keypress(function(e) {
+    // IE & Chrome capture characters and return.
+    // Mozilla captures all.
+    // IE captures on keyCode.
+    if ($.browser.mozilla && !e.charCode) return;
+    charCode = e.charCode || e.keyCode;
+    if (charCode == 13) return false;
+    scrollToEnd();
+
+    if (!(e.metaKey || e.ctrlKey || e.altKey)) {
       var char = String.fromCharCode(charCode);
       $spanLeft.append(char);
-      if (e.metaKey || e.ctrlKey || e.altKey){
-        backSpace();
-        return true;
-      }
       return false;
-    })
+    }
+  });
 
-    [keydown](function(e){
-      var char;
-      switch (e.keyCode){
-       case 38:
+  // Mozilla always prefers keypress.
+  $typer[$.browser.mozilla ? 'keypress' : 'keydown'](function(e) {
+    switch (e.keyCode) {
+      case 38:
         getPast();
         break;
-       case 40:
+      case 40:
         getFuture();
         break;
-       case 39:
+      case 39:
         moveRight();
+        return false;
         break;
       case 37:
         moveLeft();
+        return false;
         break;
       case 8:
         backSpace();
@@ -80,27 +84,31 @@ $.fn.console = function(options){
         return false;
         break;
       case 13:
-        enter(); 
+        enter();
         return false;
-        break;      
-      }
-    })
-    .bind('paste', function(e){
-      setTimeout(function(){
-        $spanLeft.append($typer.val());
-        $prompt.focus();
-        $typer.val('');
-        $typer.focus();
-      },0);
-    });
-//end prompt bindings
-  var empty = function(){
+        break;
+    }
+  });
+
+  // Special tratment of pasting.
+  $typer.bind('paste', function(e) {
+    setTimeout(function(){
+      $spanLeft.append($typer.val());
+      $prompt.focus();
+      $typer.val('');
+      $typer.focus();
+    },0);
+  });
+
+  // Empties the current prompt.
+  var empty = function() {
     $cursor.empty();
     $spanLeft.empty();
     $spanRight.empty();
   };
 
-  var moveRight = function(){
+  // Moves the cursor to the right.
+  var moveRight = function() {
     var currentChar = $cursor.text(); 
     if (!currentChar.length) return;
     var text = $spanRight.text();
@@ -110,7 +118,8 @@ $.fn.console = function(options){
     $spanRight.text(text.substr(1));
   };
 
-  var moveLeft = function(){
+  // Moves the cursor to the left.
+  var moveLeft = function() {
     var currentChar = $cursor.text();
     var text = $spanLeft.text();
     if (!text.length) return;
@@ -119,15 +128,18 @@ $.fn.console = function(options){
     $cursor.text(char);
     $spanLeft.text(text.substr(0, text.length - 1));
   };
-  
-  var backSpace = function(){
+
+  // Removes the last character before the cursor.
+  var backSpace = function() {
     var text = $spanLeft.text();
     if (!text.length) return;
     $spanLeft.text(text.substr(0, text.length - 1));
   };
-  
-  var enterFactory = function(label, callback, remember){
-    return function(){
+
+  // Creates a new input prompt.
+  // TODO(max99x): Burn it with fire!
+  var enterFactory = function(label, callback, remember) {
+    return function() {
       var command = $prompt.text()
       $historyItem.clone().find('pre').html(command).end().appendTo($history);
       command = command.substr(label.length);
@@ -143,31 +155,31 @@ $.fn.console = function(options){
   var _enter;
   var enter = _enter = enterFactory(settings.label, settings.handler, true);
   
-  var tab = function(){
+  var tab = function() {
     $spanLeft.append('    ');  
   };
   
-  var stdout = function(text){
+  var stdout = function(text) {
     // Lazy appending. I.e. on the first call to out append the output field
     if (!currentOut.parent().is($history)) currentOut.appendTo($history);
     currentOut.find('pre').append(text);
     scrollToEnd();
   };
 
-  var result = function (text){
+  var result = function (text) {
     text = text || '';
     $historyItem.clone().find('pre').html(text).end().appendTo($history); 
     currentOut = null;
     scrollToEnd();
   };
   
-  var getPast = function(){
+  var getPast = function() {
     if (!history_index) return;
     history_index--;
     setContent(history[history_index]);
   };
 
-  var getFuture = function(){
+  var getFuture = function() {
     if (history_index >= history.length - 1) {
       empty();
       return;
@@ -176,61 +188,36 @@ $.fn.console = function(options){
     setContent(history[history_index]);
   };
   
-  var setContent = function(text){
+  var setContent = function(text) {
     empty();
     $spanLeft.text(text);
     scrollToEnd();
   }
 
-  var scrollToEnd = function(wait){
-    var scroll = function(){that.scrollTop(that[0].scrollHeight);};
-    if (wait)
-      setTimeout(scroll, 100);
-    else
-      scroll();
+  var scrollToEnd = function(wait) {
+    that.scrollTop(that[0].scrollHeight);
   }
-  var stdinQueue = [];
-  var stdinInFlight = false;
-  // Public API
+
+  // Public API.
   $.extend(this, {
-    stdin: function(label, callback){
-      var cb = function (){
-        callback.apply(this,[].slice.call(arguments));
-        stdinInFlight = false;
-        check();
-      };
-      var qItem = function(){
-          stdinInFlight = true;
-          $promptLabel.text(label);
-          enter = enterFactory(label, cb);
-       
-      };
-      function check(){
-        setTimeout(function(){
-          if (!stdinInFlight && stdinQueue.length) stdinQueue.shift()();
-        }, 100);
-      }
-      stdinQueue.push(qItem);
-      check();
-  },
-    setText: function(text){
-      if (text){
-        setContent(text);
-      }
+    stdin: function(callback) {
+      setTimeout(function() {
+        enter = enterFactory('', callback);
+      }, 100);
     },
-    reset: function(){
+    setText: setContent,
+    reset: function() {
       empty();
       $history.empty();
+      $('<pre/>').text(settings.greetings).appendTo($history);
     },
     scrollToEnd: scrollToEnd
-
   }); 
   
+  // Save this in data to be accessed if lost.
+  $(this).data("console", this);
 
   return this;
 };
 
 })(jQuery);
-
-
-
